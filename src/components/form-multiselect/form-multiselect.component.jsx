621f3780eb150ee00e2a5889a9331314a3dc2b36
Niall Byrne <niall@niallbyrne.ca>
2020-06-17 17:27:54 -0400

    refactor(FORMS): refactor form import to react-bootstrap spec

--- src/components/form-multiselect/form-multiselect.component.jsx
+++ src/components/form-multiselect/form-multiselect.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Select from "react-select";
-import { Form } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
 
 const MultiDropDown = ({
   setErrorMsg,
