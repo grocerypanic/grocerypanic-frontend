3fb31a5ea371f2ada72829dd853d5c26e166c74e
Niall Byrne <niall@niallbyrne.ca>
2020-06-21 15:32:09 -0400

    feat(TRANSACTION.PROVIDER): wire transaction provider into the root provider

--- src/providers/root.provider.js
+++ src/providers/root.provider.js
@@ -3,9 +3,12 @@
 import React from "react";
 
 import AnalyticsProvider from "./analytics/analytics.provider";
+
 import ItemProvider from "./api/item/item.provider";
 import ShelfProvider from "./api/shelf/shelf.provider";
 import StoreProvider from "./api/store/store.provider";
+import TransactionProvider from "./api/transaction/transaction.provider";
+
 import UserProvider from "./user/user.provider";
 
 const RootProvider = ({ children }) => {
@@ -14,7 +17,9 @@ const RootProvider = ({ children }) => {
       <AnalyticsProvider>
         <ItemProvider>
           <ShelfProvider>
-            <StoreProvider>{children}</StoreProvider>
+            <StoreProvider>
+              <TransactionProvider>{children}</TransactionProvider>
+            </StoreProvider>
           </ShelfProvider>
         </ItemProvider>
       </AnalyticsProvider>
--- src/providers/tests/root.provider.test.js
+++ src/providers/tests/root.provider.test.js
@@ -4,15 +4,28 @@ import { render, cleanup } from "@testing-library/react";
 import RootProvider from "../root.provider";
 
 import AnalyticsProvider from "../analytics/analytics.provider";
+
+import ItemProvider from "../api/item/item.provider";
 import ShelfProvider from "../api/shelf/shelf.provider";
+import StoreProvider from "../api/store/store.provider";
+import TransactionProvider from "../api/transaction/transaction.provider";
+
 import UserProvider from "../user/user.provider";
 
 jest.mock("../analytics/analytics.provider");
+jest.mock("../api/item/item.provider");
 jest.mock("../api/shelf/shelf.provider");
+jest.mock("../api/store/store.provider");
+jest.mock("../api/transaction/transaction.provider");
 jest.mock("../user/user.provider");
 
 AnalyticsProvider.mockImplementation(({ children }) => children);
+
+ItemProvider.mockImplementation(({ children }) => children);
 ShelfProvider.mockImplementation(({ children }) => children);
+StoreProvider.mockImplementation(({ children }) => children);
+TransactionProvider.mockImplementation(({ children }) => children);
+
 UserProvider.mockImplementation(({ children }) => children);
 
 let utils;
@@ -30,7 +43,11 @@ afterEach(cleanup);
 
 it("should render with the correct message", () => {
   expect(AnalyticsProvider).toHaveBeenCalledTimes(1);
+
   expect(ShelfProvider).toHaveBeenCalledTimes(1);
-  expect(UserProvider).toHaveBeenCalledTimes(1);
+  expect(ItemProvider).toHaveBeenCalledTimes(1);
+  expect(StoreProvider).toHaveBeenCalledTimes(1);
+  expect(TransactionProvider).toHaveBeenCalledTimes(1);
+
   expect(utils.findByText("Missing Ingredients")).toBeTruthy();
 });
