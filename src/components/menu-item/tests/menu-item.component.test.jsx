1fa7484521b23f2f65b296ddfd9033e5ee0db02b
Niall Byrne <niall@niallbyrne.ca>
2020-06-30 10:34:05 -0400

    test(MENU.ITEM): enforce component styling with snapshots

--- src/components/menu-item/tests/menu-item.component.test.jsx
+++ src/components/menu-item/tests/menu-item.component.test.jsx
@@ -12,27 +12,26 @@ const props = {
 };
 
 describe("Setup Environment", () => {
-  let tests = [1, 2];
   let utils;
-  let currentTest;
 
   beforeEach(() => {
     jest.clearAllMocks();
-    currentTest = tests.shift();
     utils = render(<MenuItem {...props} />);
   });
 
   afterEach(cleanup);
 
   it("should render with the correct message", () => {
-    expect(currentTest).toBe(1);
     expect(utils.getByText(props.name)).toBeTruthy();
   });
 
   it("should handle a click correctly", () => {
-    expect(currentTest).toBe(2);
     const node = utils.getByTestId("MenuElement");
     fireEvent.click(node, "click");
     expect(props.choose).toHaveBeenCalledWith(props.location);
+  });
+
+  it("should match the snapshot on file (styles)", () => {
+    expect(utils.container.firstChild).toMatchSnapshot();
   });
 });
