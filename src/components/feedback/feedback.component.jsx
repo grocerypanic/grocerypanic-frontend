6f55356921ef6972add803c417dac608eb397461
Niall Byrne <niall@niallbyrne.ca>
2020-07-02 14:34:13 -0400

    fix(FEEDBACK): fix/enable translation in this component

--- src/components/feedback/feedback.component.jsx
+++ src/components/feedback/feedback.component.jsx
@@ -4,7 +4,6 @@ import { useTranslation } from "react-i18next";
 import { AnalyticsActions } from "../../providers/analytics/analytics.actions";
 import { AnalyticsContext } from "../../providers/analytics/analytics.provider";
 
-import Strings from "../../configuration/strings";
 import { External } from "../../configuration/routes";
 
 import { TextBox, StyledLink } from "./feedback.styles.jsx";
@@ -19,17 +18,17 @@ const FeedBack = () => {
 
   return (
     <TextBox data-testid="FeedBack">
-      <span>{t(Strings.FeedBack.Request1)}</span>
+      <span>{t("FeedBack.Request1")}</span>
       <span data-testid={"FeedBackLink"}>
-        {t(Strings.FeedBack.Request2)}
+        {t("FeedBack.Request2")}
         <StyledLink
           target={"_blank"}
           href={External.feedback}
           onClick={handleClick}
         >
-          {t(Strings.FeedBack.Link)}
+          {t("FeedBack.Link")}
         </StyledLink>
-        {t(Strings.FeedBack.Request3)}
+        {t("FeedBack.Request3")}
       </span>
     </TextBox>
   );
