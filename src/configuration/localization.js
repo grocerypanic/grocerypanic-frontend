9d91b672084db8efd77112861209d3526b838221
Niall Byrne <niall@niallbyrne.ca>
2020-07-02 16:49:39 -0400

    feat(LOCALIZATION): conditional debugging of translation based on node environment

--- src/configuration/localization.js
+++ src/configuration/localization.js
@@ -2,11 +2,14 @@ import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { Strings } from "./strings";
 
+const isNotProductionOrTest = () =>
+  process.env.NODE_ENV !== "production" && process.env.NODE_ENV !== "test";
+
 const i18nConfig = {
   fallbackLng: {
     default: ["en"],
   },
-  debug: true,
+  debug: isNotProductionOrTest() ? true : false,
   ns: ["translation"],
   defaultNS: "translation",
   keySeparator: ".",
