8b88cb826e2d379a2082de4e27c0a173bc749fa1
Niall Byrne <niall@niallbyrne.ca>
2020-07-20 11:31:55 -0400

    feat(ASSETS): add royalty free images for splash page, keep a note to buy the authors a coffee

--- src/configuration/assets.js
+++ src/configuration/assets.js
@@ -1,6 +1,8 @@
 const Assets = {
   favicon: `${process.env.PUBLIC_URL}/favicon.ico`,
   nonBreakingSpace: "\u00A0",
+  cart: `${process.env.PUBLIC_URL}/images/cart.jpg`,
+  bill: `${process.env.PUBLIC_URL}/images/bill.jpg`,
 };
 
 export default Assets;
