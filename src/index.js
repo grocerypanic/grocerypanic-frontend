02eda047b4ebef36098a0cb8e76191d52c4e1316
Niall Byrne <niall@niallbyrne.ca>
2020-11-27 10:20:16 -0500

    feat(SERVICEWORKER): manage pwa updates via the service worker

--- src/index.js
+++ src/index.js
@@ -36,4 +36,18 @@ ReactDOM.render(<Index />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+
+export const performUpdate = (registration) => {
+  const confirmation = window.confirm(
+    "New version available!  Ready to update?"
+  );
+  if (!confirmation) return;
+  if (registration && registration.waiting) {
+    registration.waiting.postMessage({ type: "SKIP_WAITING" });
+  }
+  window.location.reload();
+};
+
+serviceWorker.register({
+  onUpdate: (registration) => performUpdate(registration),
+});
--- src/tests/index.test.jsx
+++ src/tests/index.test.jsx
@@ -32,11 +32,66 @@ describe("Setup Test", () => {
     process.env = environment;
   });
 
-  it("should call render on the index object, and register the service worker as expected", () => {
-    const { Index } = require("../index.js");
-    expect(ReactDOM.render).toHaveBeenCalledTimes(1);
-    expect(ReactDOM.render).toHaveBeenCalledWith(<Index />, div);
-    expect(serviceWorker.unregister).toHaveBeenCalledTimes(0);
-    expect(serviceWorker.register).toHaveBeenCalledTimes(1);
+  describe("when the update confirmation is accepted", () => {
+    let mockRegistration;
+    beforeEach(() => {
+      jest.spyOn(global, "confirm").mockReturnValueOnce(true);
+      mockRegistration = {
+        waiting: {
+          postMessage: jest.fn(),
+        },
+      };
+      delete window.location;
+      window.location = { ...window.location, reload: jest.fn() };
+    });
+
+    it("should call render on the index object, and register the service worker and perform updates as expected", () => {
+      const { Index } = require("../index.js");
+      expect(ReactDOM.render).toHaveBeenCalledTimes(1);
+      expect(ReactDOM.render).toHaveBeenCalledWith(<Index />, div);
+      expect(serviceWorker.unregister).toHaveBeenCalledTimes(0);
+      expect(serviceWorker.register).toHaveBeenCalledTimes(1);
+
+      const registerFn = serviceWorker.register.mock.calls[0][0];
+      registerFn.onUpdate({});
+      expect(window.location.reload).toBeCalledTimes(1);
+    });
+
+    it("when perform update is called it should post the SKIP_WAITING message and reload the page", () => {
+      const { performUpdate } = require("../index.js");
+      performUpdate({});
+      expect(mockRegistration.waiting.postMessage).toBeCalledTimes(0);
+      expect(window.location.reload).toBeCalledTimes(1);
+    });
+
+    it("when perform update is called it should reload the page but not post the message if there is not a waiting object", () => {
+      const { performUpdate } = require("../index.js");
+      performUpdate(mockRegistration);
+      expect(mockRegistration.waiting.postMessage).toBeCalledWith({
+        type: "SKIP_WAITING",
+      });
+      expect(window.location.reload).toBeCalledTimes(1);
+    });
+  });
+
+  describe("when the update confirmation is not accepted", () => {
+    let mockRegistration;
+    beforeEach(() => {
+      jest.spyOn(global, "confirm").mockReturnValueOnce(false);
+      mockRegistration = {
+        waiting: {
+          postMessage: jest.fn(),
+        },
+      };
+      delete window.location;
+      window.location = { ...window.location, reload: jest.fn() };
+    });
+
+    it("should not perform updates", () => {
+      const { performUpdate } = require("../index.js");
+      performUpdate(mockRegistration);
+      expect(mockRegistration.waiting.postMessage).toBeCalledTimes(0);
+      expect(window.location.reload).toBeCalledTimes(0);
+    });
   });
 });
