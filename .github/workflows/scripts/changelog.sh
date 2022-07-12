032b88dd25c0df7ae6a8ae9999813c89aedad153
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2022-07-11 22:30:30 -0400

    ci(GITHUB): add changelog script

---
+++ .github/workflows/scripts/changelog.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eo pipefail
+
+main() {
+
+  TAG="$(git tag | sort --version-sort | tail -n 2 | head -n 1)"
+  CHANGE_LOG_CONTENT="$(npx -q generate-changelog -f - -t "${TAG}")"
+
+  {
+    echo "CHANGE_LOG_CONTENT<<EOF"
+    echo "${CHANGE_LOG_CONTENT}"
+    echo "EOF"
+  } >> "$GITHUB_ENV"
+
+}
+
+main
